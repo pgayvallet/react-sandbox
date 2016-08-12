048db1b378703039c45c0a42c038f9ec421415ad
pierre_gayvallet <pgayvallet.prestataire@voyages-sncf.com>
2016-08-12 10:22:09 +0200

    moving Home to own file to allow hot reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
-import { browserHistory, Router, Route, Link } from 'react-router';
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { browserHistory, Router, Route } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import ModalPortal from "./modal/modal-portal";
-import { connect } from 'react-redux';
+import Home from "./home/Home";
+
 import { Topbar } from "./topbar/topbar";
 
 import store from "./store";
@@ -12,49 +12,6 @@ import store from "./store";
 // import * as Promise from "bluebird";
 
 const history = syncHistoryWithStore(browserHistory, store);
-
-
-//require("./main.scss");
-
-class _Home extends React.Component<any, any> {
-
-    props : any;
-
-    render() {
-        return (
-            <div>
-                Ceci est la home !
-                <button onClick={this.openPopin.bind(this)}>Ouvrir la popin</button>
-            </div>
-        );
-    }
-
-    openPopin() {
-        console.log("open popin");
-        console.log(this.props.dispatch, this.props.openTestPopin);
-        this.props.openTestPopin();
-    }
-
-}
-
-import { openModal } from "./modal/modal-action-creators";
-import {openConfirmDialog, ConfirmDialogOptions} from "./modal/confirmDialog";
-
-const Home = connect(
-    () => {
-        return {};
-    },
-    (dispatch) => {
-        return {
-            openTestPopin : () => {
-                dispatch(openConfirmDialog({
-                    confirmAction : openConfirmDialog({text : "confirmation's confirmation !"})
-                }));
-            },
-        };
-    })
-(_Home);
-
 
 class ApplicationLayout extends React.Component<any, any> {
 
---
+++ src/home/Home.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { connect } from 'react-redux';
+
+import { openConfirmDialog } from "../modal/confirmDialog";
+
+let mapStateToProps = (state, props) => {
+    return {};
+};
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        openTestPopin : () => {
+            dispatch(openConfirmDialog({
+                confirmAction : openConfirmDialog({text : "confirmation's confirmation !"})
+            }));
+        },
+    };
+};
+
+class Home extends React.Component<any, any> {
+
+    props : any;
+
+    render() {
+        return (
+            <div>
+                Ceci est la home :)
+                <button onClick={this.openPopin.bind(this)}>Ouvrir la popin</button>
+            </div>
+        );
+    }
+
+    openPopin() {
+        //console.log("open popin");
+        //console.log(this.props.dispatch, this.props.openTestPopin);
+        this.props.openTestPopin();
+    }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
